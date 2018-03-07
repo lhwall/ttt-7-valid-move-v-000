731963ed045a929c8612bec888c2ee5ad7795481
Lucy Wall <lhwall@gmail.com>
2018-03-07 16:14:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?
-  if index
+  if index < 1 || index < 9
+    false
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?
