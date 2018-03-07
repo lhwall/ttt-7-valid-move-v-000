44ef54c7427b200ce68126c1fb6744d91648d15b
Lucy Wall <lhwall@gmail.com>
2018-03-07 16:13:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,13 @@
 # code your #valid_move? method here
 
 
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
+  if board[index] == " "
+    FALSE
+  elsif board[index] == "" || board[index] == NIL
+    FALSE
+  else
+    TRUE
+  end
